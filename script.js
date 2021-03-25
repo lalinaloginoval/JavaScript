407e7e55ff9012eec0cee7feb965b5a26334733c
lalinaloginoval <alina.login30@gmail.com>
2021-03-25 11:53:15 +0300

    fix

---
+++ script.js
@@ -0,0 +1,16 @@
+// задание 1
+function temperatureFahrenheit(tc) {
+    return (9 / 5) * tc + 32;
+}
+
+alert('Температура по Фаренгейту - ' + temperatureFahrenheit(10) + ' градусов');
+
+
+// задание 2
+let nameUser = 'Василий';
+let admin = nameUser;
+
+alert('Admin - ' + admin);
+
+// задание 3
+console.log(1000 + "108") // = "1000108" - просто конкатенация 
